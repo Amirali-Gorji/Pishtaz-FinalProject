b3830ca010add1d6f6443c3e61ee761ed97e77e9
amirali <amirali.gorji@gmail.com>
2020-09-21 08:10:06 +0330

    third analyse modified

--- Python Program/GainData.py
+++ Python Program/GainData.py
@@ -47,7 +47,7 @@ for row in result:
         else:
             ((mapONE[row[7]])[row[2]])[0] = ((mapONE[row[7]])[row[2]])[0] + row[5]
 
-print("most popular product in every provice\n\t")
+print("1-Most popular product in every provice\n\t")
 #most popular product in every provice
 for item in mapONE:
     tmpDIC = mapONE[item]
@@ -60,7 +60,7 @@ for item in mapONE:
             id = productID
             price = ((mapONE[item])[productID])[1]
     countryMap[id] = [item,max,price]
-    print(f'in {item}\n\t\tmost popular product is : {id}\n\t\twhich sold {max} number of item\n\t\tand whole profit is : {price*max}')
+    print(f'In {item}\n\t\tmost popular product is : {id}\n\t\twhich sold {max} number of items\n\t\tand total profit is : {price*max}')
 
 print("\n\n\n")
 #most popular product in whole country 
@@ -74,65 +74,60 @@ for item in countryMap:
         provice = (countryMap[item])[0]
         id = item
         price = (countryMap[item])[2]
-print("most popular product in whole country\n")
-print(f'\tIn whole country\n\t\tmost popular product is : {id}\n\t\twhich sold {max} number of item\n\t\tin {provice}\n\t\tprofit is : {price*max}')
+print("2-Most popular product in whole country\n")
+print(f'\tIn whole country\n\t\tmost popular product is : {id}\n\t\twhich sold {max} number of items\n\t\tin {provice}\n\t\tprofit is : {price*max}')
 
     
 
 
-# for row in result:
-#     print(f'{row[6]}\n')
 
 #most popular product in every city  
 for row in result:
    
-    if(not(row[7] in mapTWO)):
-        mapTWO[row[7]] = {}
-        if( not (row[6] in mapTWO[row[7]])):
-            (mapTWO[row[7]])[row[6]] = {}
-            if(not( row[2] in (mapTWO[row[7]])[row[6]])):
-                ((mapTWO[row[7]])[row[6]])[row[2]] = [row[5],row[3]]
+    if(not(row[6] in mapTWO)):
+        mapTWO[row[6]] = {}
+        if( not (row[7] in mapTWO[row[6]])):
+            (mapTWO[row[6]])[row[7]] = {}
+            if(not( row[2] in (mapTWO[row[6]])[row[7]])):
+                ((mapTWO[row[6]])[row[7]])[row[2]] = [row[5],row[3]]
             else:
-                (((mapTWO[row[7]])[row[6]])[row[2]])[0] = (((mapTWO[row[7]])[row[6]])[row[2]])[0] + row[5]
+                (((mapTWO[row[6]])[row[7]])[row[2]])[0] = (((mapTWO[row[6]])[row[7]])[row[2]])[0] + row[5]
         else:
-            if(not( row[2] in (mapTWO[row[7]])[row[6]])):
-                ((mapTWO[row[7]])[row[6]])[row[2]] = [row[5],row[3]]
+            if(not( row[2] in (mapTWO[row[6]])[row[7]])):
+                ((mapTWO[row[6]])[row[7]])[row[2]] = [row[5],row[3]]
             else:
-                (((mapTWO[row[7]])[row[6]])[row[2]])[0] = (((mapTWO[row[7]])[row[6]])[row[2]])[0] + row[5]
+                (((mapTWO[row[6]])[row[7]])[row[2]])[0] = (((mapTWO[row[6]])[row[7]])[row[2]])[0] + row[5]
 
     else:
-        if( not (row[6] in mapTWO[row[7]])):
-            (mapTWO[row[7]])[row[6]] = {}
-            if(not( row[2] in (mapTWO[row[7]])[row[6]])):
-                ((mapTWO[row[7]])[row[6]])[row[2]] = [row[5],row[3]]
+        if( not (row[7] in mapTWO[row[6]])):
+            (mapTWO[row[6]])[row[7]] = {}
+            if(not( row[2] in (mapTWO[row[6]])[row[7]])):
+                ((mapTWO[row[6]])[row[7]])[row[2]] = [row[5],row[3]]
             else:
-                (((mapTWO[row[7]])[row[6]])[row[2]])[0] = (((mapTWO[row[7]])[row[6]])[row[2]])[0] + row[5]
+                (((mapTWO[row[6]])[row[7]])[row[2]])[0] = (((mapTWO[row[6]])[row[7]])[row[2]])[0] + row[5]
         else:
-            if(not( row[2] in (mapTWO[row[7]])[row[6]])):
-                ((mapTWO[row[7]])[row[6]])[row[2]] = [row[5],row[3]]
+            if(not( row[2] in (mapTWO[row[6]])[row[7]])):
+                ((mapTWO[row[6]])[row[7]])[row[2]] = [row[5],row[3]]
             else:
-                (((mapTWO[row[7]])[row[6]])[row[2]])[0] = (((mapTWO[row[7]])[row[6]])[row[2]])[0] + row[5]
+                (((mapTWO[row[6]])[row[7]])[row[2]])[0] = (((mapTWO[row[6]])[row[7]])[row[2]])[0] + row[5]
  
 
-for item in mapTWO:
-    for item2 in item:
-        print(f'{item2}')
-    print("\n\n")
+
 
 
 print("\n\n\n")
 #showin result in every city
-print(f'most popular product in every city:\n\t')
+print(f'3-Most popular product in every city:\n')
 for provice in mapTWO:
-    print(f'{mapTWO[provice]}\n')
-    for city in provice:
+    
+    for city in mapTWO[provice]:
         max = 0
         tmpID = 0
         price = 0
-        for id in city:
+        for id in (mapTWO[provice])[city]:
             if((((mapTWO[provice])[city])[id])[0] > max ):
                 max = (((mapTWO[provice])[city])[id])[0]
                 tmpID = id
                 price = (((mapTWO[provice])[city])[id])[1]
-        print(f'In {provice} , {city}\n\t\tmost popular product is : {tmpID}\n\t\twhich sold {max} number of item\n\t\total profit is : {price*max}')
+        print(f'\tIn {provice} , {city}\n\t\tmost popular product is : {tmpID}\n\t\twhich sold {max} number of items\n\t\ttotal profit is : {price*max}')
             
--- run.sh
+++ run.sh
@@ -1,11 +1,20 @@
 #! /bin/bash
 echo "first step:"
 chmod +x ./Script/downloadData.sh
-./Script/downloadData.sh
+
+#./Script/downloadData.sh
 echo "executing C file:"
-gcc -Wall -Wextra -I "/usr/include/postgresql" -L "/usr/lib/postgresql" ./Cprogram/workWithData.c -lpq -o ./Cprogram/work
-sudo ./Cprogram/work
+cd ./Cprogram
+
+
+gcc -Wall -Wextra -I "/usr/include/postgresql" -L "/usr/lib/postgresql" preExe.c -lpq -o ./pre.out
+sudo ./pre.out
+
+gcc -Wall -Wextra -I "/usr/include/postgresql" -L "/usr/lib/postgresql" workWithData.c -lpq -o ./work.out
+sudo ./work.out
+
 
 rm *.text
-#sleep for 30 minutes
-sleep 1800
+
+
+
